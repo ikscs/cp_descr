36cfe7b7471120fade48be58cbf306861c3f552b
olavrykov <alexander.lavrikov@gmail.com>
2025-02-04 14:14:31 +0200

    modal comp

---
+++ starter/src/components/Modal.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Modal.css';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>&times;</span>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
