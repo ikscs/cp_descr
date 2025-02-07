52a73b824c287956c192a5ade4e8deb628c3dd29
olavrykov <alexander.lavrikov@gmail.com>
2025-02-07 17:34:54 +0200

    checkbox update

--- prod-filter/src/components/checkbox.tsx
+++ prod-filter/src/components/checkbox.tsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 interface CheckboxProps {
   label: string;
-  defaultValue: boolean;
-  onChange: (value: boolean) => void
+  checked: boolean;
+  onChange: (checked: boolean) => void;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ label, defaultValue, onChange }) => {
-  const [checked, setChecked] = useState<boolean>(defaultValue);
-
+const Checkbox: React.FC<CheckboxProps> = ({ label, checked, onChange }) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked)
-    onChange(event.target.checked)
+    onChange(event.target.checked);
   };
+
   return (
-    <div style={{marginTop: 8}}>
+    <div style={{ marginTop: 8 }}>
       <label>
-        <input 
+        <input
           type="checkbox"
           checked={checked}
           onChange={handleChange}
@@ -24,22 +22,7 @@ const Checkbox: React.FC<CheckboxProps> = ({ label, defaultValue, onChange }) =>
         {label}
       </label>
     </div>
-  )
-}
+  );
+};
 
-export default Checkbox
-
-/*
-Certainly! Here’s a concise example of how you can use a checkbox input in a React component with TypeScript:
-
-In this example:
-
-We define a CheckboxProps interface to type the props.
-We use the useState hook to manage the checkbox state.
-The handleChange function updates the state when the checkbox is toggled.
-The checked attribute of the input element is bound to the state.
-
-This should give you a good starting point for using checkboxes 
-in a React component with TypeScript. If you have any specific 
-requirements or need further customization, feel free to let me know!
-*/
+export default Checkbox;
