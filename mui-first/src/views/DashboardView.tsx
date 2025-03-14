a24ada33e0758cefc2ce030128e090c1756c6b92
olavrykov <alexander.lavrikov@gmail.com>
2025-03-14 09:15:04 +0200

    Carousel added

--- mui-first/src/views/DashboardView.tsx
+++ mui-first/src/views/DashboardView.tsx
@@ -1,6 +1,25 @@
+// yarn add react-responsive-carousel
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Box } from '@mui/system';
+import './DashboardView.css';
+
+const images = [
+  
+  'https://picsum.photos/640/480?random=111',
+  'https://loremflickr.com/640/480?random=1111',
+  'https://picsum.photos/640/480?random=2233',
+  'https://loremflickr.com/640/480?random=3344',
+  'https://loremflickr.com/640/480?random=4455',
+  'https://loremflickr.com/640/480?random=5665',
+  'https://loremflickr.com/640/480?random=6677',
+  'https://loremflickr.com/640/480?random=7887',
+  'https://loremflickr.com/640/480?random=8998',
+  'https://loremflickr.com/640/480?random=9009',
+  'https://loremflickr.com/640/480?random=10110',
+];
 
 const DashboardView: React.FC = () => {
   return (
@@ -10,6 +29,17 @@ const DashboardView: React.FC = () => {
       <Box sx={{ marginTop: '20px' }}>
         <video src="/Google_Mio_SizzleGIF_3840x2160.mp4" autoPlay loop width="640" height="360" />
       </Box>
+      <Carousel
+        axis='vertical'
+        infiniteLoop={true}
+        className="half-screen-carousel"
+      >
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image} alt={`Image ${index + 1}`} className="carousel-image" />
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 };
