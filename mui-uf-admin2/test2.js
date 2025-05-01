43a9b7a3751573d4a0ae0e2d3312401ebf489e33
olavrykov <alexander.lavrikov@gmail.com>
2025-05-01 12:55:17 +0300

    test

--- mui-uf-admin2/test2.js
+++ mui-uf-admin2/test2.js
@@ -1,18 +1,21 @@
-let params = {from: "age", limit: 10};
+let params = { from: "age", limit: 10 };
 
 let body = JSON.stringify(params);
-console.log('Send\n', params);
+console.log("Send\n", params);
 
 //const json = await fetch('https://cnt.theweb.place/back/f5.php?func=public.js_echo_b', {
 
-const response = await fetch('https://cnt.theweb.place/back/f5.php?func=public.js_select_b', {
-    method: 'POST',
+const response = await fetch(
+  "https://cnt.theweb.place/back/f5.php?func=public.js_select_b",
+  {
+    method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    body
-})
+    body,
+  }
+);
 
 const receved = await response.json();
-console.log('Receive\n', receved);
+console.log("Receive\n", receved);
