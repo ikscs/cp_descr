d634b18f9b8fa6052c3d20cc076151b81f001671
olavrykov <alexander.lavrikov@gmail.com>
2025-05-20 10:44:31 +0300

    report list for dashboard

--- mui-uf-admin2/src/pages/Reports/ViewerReportList.tsx
+++ mui-uf-admin2/src/pages/Reports/ViewerReportList.tsx
@@ -6,7 +6,7 @@ import { Report } from '../../api/data/reportTools'; // Import Report type
 // Define the predicate function.
 // This example allows only reports with specific IDs.
 // You can customize this logic as needed (e.g., filter by name, category, etc.).
-const VIEWER_ALLOWED_REPORT_IDS: number[] = [26, 27, 28, 29,]; // Example: Allow reports with ID 1, 3, or 5
+const VIEWER_ALLOWED_REPORT_IDS: number[] = [26, 27, 28, 29, 30, ]; // Example: Allow reports with ID 1, 3, or 5
 
 /**
  * Predicate function to filter reports for the viewer.
